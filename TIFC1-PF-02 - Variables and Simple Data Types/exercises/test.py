c4e1ad2a7744011466b4e52578f20ab8dcdd38b1
Nas <n.choudhury1@live.co.uk>
2024-09-11 11:29:14 +0100

    variable and simple exercises

---
+++ TIFC1-PF-02 - Variables and Simple Data Types/exercises/test.py
@@ -0,0 +1,46 @@
+# 1
+bonjour = 'hello world'
+print(bonjour)
+
+# 2
+name = 'john'
+print(name)
+
+name = 'steve'
+print(name)
+
+# 3
+name = 'adam'
+print(f'hello {name}, how are you?')
+
+# 4
+name = 'king'
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+# 5
+print(4+4)
+print(12-4)
+print(4*2)
+print(16/2)
+
+# 6
+num = 13
+print(f'Can you guess what my favourite number is?')
+print(f'Nope, it\'s {num}.')
+
+# stretch challenge
+name = 'shock'
+print(name)
+
+first_name = 'bob'
+last_name = 'blue'
+print(f'Hello my name is {first_name} {last_name}')
+
+good = 'Good'
+day = 'Day'
+name = 'Nasir'
+
+printed_statement = f'{good} {day} {name}'
+print(printed_statement)
